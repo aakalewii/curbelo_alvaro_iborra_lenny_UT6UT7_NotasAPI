f6a5e93ed3c25129e221558f30d2a621818fd6d9
aakalewii <146139742+aakalewii@users.noreply.github.com>
2025-06-03 21:26:54 +0100

    Add files via upload

---
+++ actev/src/main/java/com/ut7/actev/ActevApplication.java
@@ -0,0 +1,13 @@
+package com.ut7.actev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActevApplication.class, args);
+	}
+
+}
---
+++ actev/src/test/java/com/ut7/actev/ActevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ut7.actev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
