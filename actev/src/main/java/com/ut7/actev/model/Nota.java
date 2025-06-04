2a50fde3fdad02d10efd07d6d38cb8b742ba50fc
alvaro123124 <alvarobudy12@gmail.com>
2025-06-04 18:49:20 +0100

    Entidad Nota

---
+++ actev/src/main/java/com/ut7/actev/model/Nota.java
@@ -0,0 +1,60 @@
+package com.ut7.actev.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notas")
+public class Nota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private Long contenido;
+
+    private LocalDateTime fechaCreacion;
+
+    public Nota() {
+    }
+
+    public Nota(String titulo, String contenido, LocalDateTime fechaCreacion) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+}
