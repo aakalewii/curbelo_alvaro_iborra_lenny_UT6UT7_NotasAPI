f45b1e6c3ac4de5aefa903d6158737859b090b63
aakalewii <erlewiirobot@gmail.com>
2025-06-06 17:43:14 +0100

    algunas ResponseStatusException

--- actev/src/main/java/com/ut7/actev/service/UsuarioServiceImpl.java
+++ actev/src/main/java/com/ut7/actev/service/UsuarioServiceImpl.java
@@ -4,8 +4,8 @@ import com.ut7.actev.model.Usuario;
 import com.ut7.actev.repository.UsuarioRepository;
 
 import org.springframework.transaction.annotation.Transactional;
-
-
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -30,7 +30,7 @@ public class UsuarioServiceImpl extends AbstractCrudService<Usuario, Long> imple
     @Override
     public Usuario update(Long id, Usuario usuario) {
         if (!usuarioRepository.existsById(id)) {
-            throw new IllegalArgumentException("Usuario no encontrado");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuario no encontrado");
         }
         if (usuario.getPasswordHash() != null && usuario.getPasswordHash().length() < 64) {
             usuario.hashPassword();
@@ -42,7 +42,7 @@ public class UsuarioServiceImpl extends AbstractCrudService<Usuario, Long> imple
     @Transactional
     public void deleteById(Long id) {
         if (!usuarioRepository.existsById(id)) {
-            throw new IllegalArgumentException("Usuario no encontrado");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuario no encontrado");
         }
         usuarioRepository.deleteById(id);
     }
